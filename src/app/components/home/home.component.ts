bec7c59be7a7e15ba54345f0218f953c760cbc83
Arvinth Vijayanathan <arvinth3@hotmail.com>
2018-11-17 15:55:16 -0500

    added icons to process and changed backgrounds

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -23,6 +23,27 @@ export class HomeComponent implements OnInit {
     }
   ];
 
+  process_title: String = "Our Process";
+
+  processes = [
+    {
+      title: 'Determine Requirments',
+      icon: 'fa fa-3x fa-list-ul'
+    },
+    {
+      title: 'Build Product',
+      icon: 'fa fa-3x  fa-laptop-code'
+    },
+    {
+      title: 'Review/Fix',
+      icon: 'fa fa-3x fa-screwdriver'
+    },
+    {
+      title: 'Launch',
+      icon: 'fa fa-3x fa-rocket'
+    }
+  ]
+
   contact_title: String = 'CONTACT US';
 
   constructor() { }
--- src/app/components/process/process.component.ts
+++ src/app/components/process/process.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-process',
   templateUrl: './process.component.html',
-  styleUrls: ['./process.component.scss']
+  styleUrls: ['./process.component.scss', '../skills/skills.component.scss']
 })
 export class ProcessComponent implements OnInit {
+
+  @Input() title: String = '';
+  @Input() processes: any [];
 
   constructor() { }
 
   ngOnInit() {
+    this.animationEffect();
+  }
+
+  /*
+    Interactivity to determine when an animated element in in view.
+    In view elements trigger our animation
+  */
+  animationEffect() {
+    $(document).ready(function () {
+
+      // Window and animation items
+      const animation_elements = $.find('.animation-element');
+      const web_window = $(window);
+
+      // Check to see if any animation containers are currently in view
+      function check_if_in_view() {
+        // Get current window information
+        const window_height = web_window.height();
+        const window_top_position = web_window.scrollTop();
+        const window_bottom_position = (window_top_position + window_height);
+
+        // Iterate through elements to see if its in view
+        $.each(animation_elements, function () {
+
+          // Get the element sinformation
+          const element = $(this);
+          const element_height = $(element).outerHeight();
+          const element_top_position = $(element).offset().top;
+          const element_bottom_position = (element_top_position + element_height);
+
+          // Check to see if this current container is visible
+          // Its viewable if it exists between the viewable space of the viewport
+          if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+            element.addClass('in-view');
+          } else {
+            element.removeClass('in-view');
+          }
+        });
+
+      }
+
+      // On or scroll, detect elements in view
+      $(window).on('scroll resize', function () {
+        check_if_in_view();
+      });
+
+      // Trigger our scroll event on initial load
+      $(window).trigger('scroll');
+
+    });
   }
 
 }
